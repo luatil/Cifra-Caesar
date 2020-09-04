ee48f6d3e7ceb1d4b769731485aa06103033ea38
luatil <luasantilli@gmail.com>
2020-09-04 15:21:01 -0300

    Simplify switch statement assignments
    This commit change the two lines of the form:
        p++;
        *p = 'x';
    To the simpler form:
        *++p = 'x';
    
    Signed-off-by: luatil <luasantilli@gmail.com>

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 char * especiais_para_letras(char *frase) {
     char *p;
     char *init = p;
-    char c;
     while(*frase) {
         switch(*frase) {
             case ' ':
@@ -11,33 +10,27 @@ char * especiais_para_letras(char *frase) {
                 break;
             case ',':
                 *p = 'v';
-                p++;
-                *p = 'r'; 
+                *++p = 'r'; 
                 break;
             case '.':
                 *p = 'p';
-                p++;
-                *p = 't';
+                *++p = 't';
                 break;
             case ':':
                 *p = 'd';
-                p++;
-                *p = 'p';
+                *++p = 'p';
                 break;
             case '!':
                 *p = 'e';
-                p++;
-                *p = 'x';
+                *++p = 'x';
                 break;
             case '?':
                 *p = 'i';
-                p++;
-                *p = 'n';
+                *++p = 'n';
                 break;
             case '\n':
                 *p = 'n';
-                p++;
-                *p = 'l';
+                *++p = 'l';
                 break;
             default:
                 *p = *frase;
@@ -49,7 +42,7 @@ char * especiais_para_letras(char *frase) {
 }
 
 int main() {
-    char *d = "Para: um?! bom\n ...entendedor, meia palavra basta.";
+    char *d = "Para um bom entendedor, meia palavra basta.";
     char *p = especiais_para_letras(d);
     printf("%s\n", d);
     printf("%s\n", p);
