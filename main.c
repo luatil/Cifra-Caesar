7583d204787fe000a2613b3daba0b07d0228fb60
luatil <luasantilli@gmail.com>
2020-09-04 15:11:17 -0300

    Make function that transforms punctuation
    Signed-off-by: luatil <luasantilli@gmail.com>

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+char * especiais_para_letras(char *frase) {
+    char *p;
+    char *init = p;
+    char c;
+    while(*frase) {
+        switch(*frase) {
+//            case ' ':
+//                p++;
+//                break;
+            case ',':
+                *p = 'v';
+                p++;
+                *p = 'r'; 
+                break;
+            case '.':
+                *p = 'p';
+                p++;
+                *p = 't';
+                break;
+            case ':':
+                *p = 'd';
+                p++;
+                *p = 'p';
+                break;
+            case '!':
+                *p = 'e';
+                p++;
+                *p = 'x';
+                break;
+            case '?':
+                *p = 'i';
+                p++;
+                *p = 'n';
+                break;
+            case '\n':
+                *p = 'n';
+                p++;
+                *p = 'l';
+                break;
+            default:
+                *p = *frase;
+        } 
+        p++;
+        frase++;
+    }
+    return init;
+}
+
+int main() {
+    char *d = "Para: um?! bom\n ...entendedor, meia palavra basta.";
+    char *p = especiais_para_letras(d);
+    printf("%s\n", d);
+    printf("%s\n", p);
+    return 0;
+}
