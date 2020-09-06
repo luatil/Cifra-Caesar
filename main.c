93a853ce2c0c9afec43c2cc414f256698ee9772f
luatil <luasantilli@gmail.com>
2020-09-06 08:35:17 -0300

    Create rotacao function
    Signed-off-by: luatil <luasantilli@gmail.com>

--- main.c
+++ main.c
@@ -41,10 +41,33 @@ char * especiais_para_letras(char *frase) {
     return init;
 }
 
+
+// d1 aplica uma rotação para as maiúsculas
+// d2 aplica uma rotação para as minúsculas
+char * rotacao(char *frase2, char d1, char d2) {
+    char *p2;
+    char *init2 = p2;
+    while(*frase2) {
+        if(*frase2 >= 'A' && *frase2 <= 'Z')
+            *p2 = *frase2 + d1;
+        else if(*frase2 >= 'a' && *frase2 <= 'z')
+            *p2 = *frase2 + d2;
+        else 
+            *p2 = *frase2;
+        p2++;
+        frase2++;
+    }
+    p2++;
+    *p2 = '\0';
+    return init2; 
+}
+
 int main() {
-    char *d = "Para um bom entendedor, meia palavra basta.";
-    char *p = especiais_para_letras(d);
-    printf("%s\n", d);
-    printf("%s\n", p);
+      char *d = "Para um bom entendedor, meia palavra basta.";
+      char *t = especiais_para_letras(d);
+//      char *f = rotacao("Para um bom" , 3, 5);
+//      printf("%s\n", f);
+      printf("%s\n", t);
     return 0;
+
 }
